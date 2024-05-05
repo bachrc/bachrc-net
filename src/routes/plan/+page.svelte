bccb00ab2f0142b142482cd7cd4f0895a44b3765
bachrc <yohann.bacha@bachrc.net>
2024-05-05 17:32:38 +0200

    fix: agencement non responsive

--- src/routes/plan/+page.svelte
+++ src/routes/plan/+page.svelte
@@ -2,8 +2,11 @@
   import { t } from 'svelte-i18n';
 </script>
 
-<div class="sm:container sm:mx-auto p-4 bg-indigo-100/50 rounded-xl">
-  <h1 class="text-2xl">{$t('plan-du-site')}</h1>
+<div class="w-full mx-auto container sm:w-1/2 p-4 bg-indigo-100/50 rounded-xl">
+  <h1 class="text-2xl pb-2 mb-2 border-b border-b-indigo-300">
+    {$t('plan-du-site')}
+  </h1>
+
   <ul class=" list-disc list-inside text-lg">
     <li><a href="/">{$t('home')}</a></li>
     <li><a href="/a-propos">{$t('a-propos')}</a></li>
